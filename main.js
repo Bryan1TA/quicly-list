d703bebf06a25c20df6a87f3fd10be741a582719
bryan21c <bryanederson19@gmail.com>
2025-06-08 20:24:09 -0300

    criação da função add

--- main.js
+++ main.js
@@ -24,3 +24,20 @@ function addItem() {
     sectionList.textContent = ""
 
     items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+    items.map((item, index) => {
+        sectionList.innerHTML +=  `
+        <div class="item">
+            <div>
+              <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
+                <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                  <img src="./assets/checked.svg" alt="checked">
+                </div>
+                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+            </div>
+
+            <button onclick="removeItem('${item.name}')">
+              <img src="./assets/trash-icon.svg" alt="trash icon">
+            </button>
+          </div>
+        `
+    })""
